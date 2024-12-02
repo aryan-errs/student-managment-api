cc21de8330785acf20e1cd2050c98c87315e8d69
Aryan Sharma <aryan182282@gmail.com>
2024-12-03 04:31:40 +0530

    added port binding

--- app/main.py
+++ app/main.py
@@ -11,3 +11,8 @@ app.include_router(students.router, prefix="/students", tags=["Students"])
 async def root():
     return {"message": "Welcome to the Student Managment API!"}
 
+
+if __name__ == "__main__":
+    # Get the port from the environment or use 8000 as a fallback
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
